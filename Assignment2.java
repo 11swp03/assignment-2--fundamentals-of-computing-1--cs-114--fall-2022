09ec87b602873b3efd9f9813026baa7073919587
11jwp03 <11swp03@gmail.com>
2022-09-28 14:06:11 -0400

    gotta pause

--- Assignment2.java
+++ Assignment2.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Assignment2 {
     public static void main(String[] args) {
-        
+		int dimondsize;
+        Scanner dimondsizeinput = new Scanner(System.in);
+		System.out.print("Enter the number of times the message should be printed: ");
+		dimondsize = dimondsizeinput.nextInt();
+		
+
+        for(int count = 1;count <= dimondsize ;){
+
+		}
     }
 }
