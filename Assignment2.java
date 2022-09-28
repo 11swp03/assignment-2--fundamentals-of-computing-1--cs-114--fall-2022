9f0c0b01f31bd6ad0f937e2095f6ea087dfb2b13
11jwp03 <11swp03@gmail.com>
2022-09-28 15:43:15 -0400

    change string

--- Assignment2.java
+++ Assignment2.java
@@ -4,12 +4,12 @@ public class Assignment2 {
     public static void main(String[] args) {
 		int dimondsize;
         Scanner dimondsizeinput = new Scanner(System.in);
-		System.out.print("Enter the number of times the message should be printed: ");
+		System.out.print("Enter the size of dimond: ");
 		dimondsize = dimondsizeinput.nextInt();
 		
 
         for(int count = 1;count <= dimondsize ;){
-
+            System.out.println("*");
 		}
     }
 }
