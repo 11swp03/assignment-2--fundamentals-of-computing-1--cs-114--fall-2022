3b93895531e04e4747acc0b4b55c7b5fec4fc24d
11swp03 <papallo@hartford.edu>
2022-09-30 18:53:26 -0400

    idk

--- Assignment2.java
+++ Assignment2.java
@@ -6,21 +6,32 @@ public class Assignment2 {
         Scanner dimondsizeinput = new Scanner(System.in);
 		System.out.print("Enter the size of dimond: ");
 		dimondsize = dimondsizeinput.nextInt();
-		
 
-        int count = 1;
-        for (int i = 1; i <= 2 * dimondsize - 1; i++) {
-            for (int j = count; j <= dimondsize; j++) {
-                System.out.print("  ");
+
+
+        for(int count = 1; count <= dimondsize; count++){
+            for(int halfdimond = dimondsize/2, count2 = 1;count2 <= halfdimond; count2++){
+                System.out.print(" ");
             }
-            for (int k = 1; k <= count * 2 - 1; k++) {
-                System.out.print("* ");
+            for(int count3 = count * 2 - 1, count4 = 1; count4 <= count3; count4++){
+                System.out.print("*");
             }
-            if (i < dimondsize)
-                count++;
-            else
-                count--;
-            System.out.println("");
+            System.out.println();
         }
+
+        // int count = 1;
+        // for (int i = 1; i <= 2 * dimondsize - 1; i++) {
+        //     for (int j = count; j <= dimondsize; j++) {
+        //         System.out.print("  ");
+        //     }
+        //     for (int k = 1; k <= count * 2 -1; k++) {
+        //         System.out.print("* ");
+        //     }
+        //     if (i < dimondsize)
+        //         count++;
+        //     else
+        //         count--;
+        //     System.out.println("");
+        // }
     }
 }
