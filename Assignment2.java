cb2f58c8526d404d22ac948a65c32674380212ae
11jwp03 <11swp03@gmail.com>
2022-10-03 10:43:29 -0400

    remove count comment

--- Assignment2.java
+++ Assignment2.java
@@ -2,21 +2,26 @@ import java.util.Scanner;
 
 public class Assignment2 {
 public static void main(String[] args) {
-
     int dimondsize;
     Scanner dimondsizeinput = new Scanner(System.in);
     System.out.print("Enter the size of dimond: ");
     dimondsize = dimondsizeinput.nextInt();
+    dimondsize = dimondsize+2;
 
-        for(int count = 0, starcount = 1; count <=dimondsize/2; count++){
-            for(int count2 = 1; count2 <= dimondsize/2; count2++){
-                System.out.print(" ");
-            }
-            for(int count3 = count+1;count3 <=starcount; count3++){
-                System.out.print("* ");
-            }
+    int count = 1;
+        for (int i = 1; i <= dimondsize; i++) {
             System.out.println("");
-            starcount = starcount + 2;
+            for (int j = count; j <= dimondsize; j++) {
+            System.out.print("  ");
+            }
+            for (int k = 1; k <= 2*count-1; k++) {
+               System.out.print("* ");
+            }
+                if (i < dimondsize/2)
+                count++;
+            else
+              count--;
         }
+        System.out.println("");
     }
 }
