0eff1618799e4f5f47f36ce4b6b1b89b5fcf0f05
11jwp03 <11swp03@gmail.com>
2022-10-03 09:41:45 -0400

    add first for loop

--- Assignment2.java
+++ Assignment2.java
@@ -8,8 +8,14 @@ public static void main(String[] args) {
 	System.out.print("Enter the size of dimond: ");
 	dimondsize = dimondsizeinput.nextInt();
 
-        for(int count = 0; count <= dimondsize/2; count++){
+        for(int count = 0, int startcount = 1; count <= dimondsize/2; count++){
+            for(int count2 = 1; count2 <= dimondsize/2; count2++){
+                System.out.println(" ");
+            }
+            for(int count3 = count+1;){
 
+            }
+            System.out.println("");
         }
     }
 }
