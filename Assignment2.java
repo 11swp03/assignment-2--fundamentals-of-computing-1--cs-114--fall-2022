387075e037a3a11ea6381ee713fd38fc1fc199e3
11jwp03 <11swp03@gmail.com>
2022-10-03 10:02:48 -0400

    jkdsfadfs

--- Assignment2.java
+++ Assignment2.java
@@ -8,11 +8,11 @@ public static void main(String[] args) {
 	System.out.print("Enter the size of dimond: ");
 	dimondsize = dimondsizeinput.nextInt();
 
-        for(int count = 0, starcount = 1; count <= dimondsize/2; count++){
+        for(int count = 0, starcount = 1; count <=dimondsize/2; count++){
             for(int count2 = 1; count2 <= dimondsize/2; count2++){
                 System.out.print(" ");
             }
-            for(int count3 = count+1;count3 <= starcount; count3++){
+            for(int count3 = count+1;count3 <=starcount; count3++){
                 System.out.print("* ");
             }
             System.out.println("");
