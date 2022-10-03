1100b1db45a590051a3b88b7bc12e3e9173f20ee
11jwp03 <11swp03@gmail.com>
2022-10-03 09:32:46 -0400

    start for loop

--- Assignment2.java
+++ Assignment2.java
@@ -2,25 +2,14 @@ import java.util.Scanner;
 
 public class Assignment2 {
 public static void main(String[] args) {
-    
+
     int dimondsize;
     Scanner dimondsizeinput = new Scanner(System.in);
 	System.out.print("Enter the size of dimond: ");
 	dimondsize = dimondsizeinput.nextInt();
 
-        int count = 1;
-        for (int i = 1; i <= 2 * dimondsize - 1; i++) {
-            for (int j = count; j <= dimondsize; j++) {
-                System.out.print("  ");
-            }
-            for (int k = 1; k <= 2 * count - 1; k++) {
-                System.out.print("* ");
-            }
-            if (i < dimondsize)
-                count++;
-            else
-                count--;
-            System.out.println("");
+        for(int count = 0; count <= dimondsize/2; count++){
+
         }
     }
 }
