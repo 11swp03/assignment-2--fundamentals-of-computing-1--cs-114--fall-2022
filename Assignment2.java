f356915a993c4db83b75230de4ef85825023dee4
11jwp03 <11swp03@gmail.com>
2022-10-03 10:46:07 -0400

    change variable names

--- Assignment2.java
+++ Assignment2.java
@@ -2,14 +2,15 @@ import java.util.Scanner;
 
 public class Assignment2 {
 public static void main(String[] args) {
+    int repeater = 1;
     int dimondsize;
     Scanner dimondsizeinput = new Scanner(System.in);
     System.out.print("Enter the size of dimond: ");
     dimondsize = dimondsizeinput.nextInt();
     dimondsize = dimondsize+2;
 
-    int count = 1;
-        for (int i = 1; i <= dimondsize; i++) {
+
+        for (int firstforloopcounter = 1; firstforloopcounter <= dimondsize; firstforloopcounter++) {
             System.out.println("");
             for (int j = count; j <= dimondsize; j++) {
             System.out.print("  ");
@@ -17,10 +18,10 @@ public static void main(String[] args) {
             for (int k = 1; k <= 2*count-1; k++) {
                System.out.print("* ");
             }
-                if (i < dimondsize/2)
-                count++;
+            if (i < dimondsize/2)
+                repeater++;
             else
-              count--;
+                repeater--;
         }
         System.out.println("");
     }
